3a39244f1f22a9dac41ee18af625a09fc27d4b11
Leyla <leylanaqiyeva940@gmail.com>
2024-10-22 15:57:44 +0400

    task2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {                                       //  ders1. test 2
+    public static void main(String[] args) {
+       // int a=0;
+        System.out.print("1-den 100-e qeder 3-e bolunen edeler: ");
+        for (int i=1; i<100; i++){
+            if(i%3==0) {
+             //   a = i;
+                System.out.print(i+", ");
+            }
+        }
+    }
+}
